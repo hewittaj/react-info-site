94cda30318b085cdadbab9f550d5e199cbc21ddc
Alex Hewitt <alexhewitt.j@gmail.com>
2023-11-16 11:43:24 -0600

    add header styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,16 @@
 import React from "react"
+import reactLogo from '../assets/react-icon-small.png'
 
 export default function Navbar() {
     return (
-        <h1>Navbar</h1>
+        <navbar className='navbar'>
+            <div className='navbar-left'>
+                <img src={reactLogo} id='react-logo'/>
+                <h1>React Facts</h1>
+            </div>
+            <div className='navbar-right'>
+                <h3>React Course - Project 1</h3>
+            </div>
+        </navbar>
     )
 }
